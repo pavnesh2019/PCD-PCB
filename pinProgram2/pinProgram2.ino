70db5e1d4a38214778a1c3af64229abbfe95747e
Pavnesh Chaturvedi <56963647+pavnesh2019@users.noreply.github.com>
2019-10-26 23:31:36 +0530

    these are temporary files 1

---
+++ pinProgram2/pinProgram2.ino
@@ -0,0 +1,24 @@
+//everytimime i push the button led inverts itself
+int ledPin=13;
+int switchPin=2;
+bool lastButton=LOW;
+bool ledOn=false;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(switchPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(switchPin)==HIGH && lastButton==LOW)
+  {
+    
+    ledOn=!ledOn;
+    lastButton=HIGH;
+  }
+  else{
+    lastButton=digitalRead(switchPin);
+  }
+  digitalWrite(ledPin,ledOn);
+}

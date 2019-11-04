d6bc0fe4a5b57529760f7a335fc89fa761659591
Pavnesh Chaturvedi <56963647+pavnesh2019@users.noreply.github.com>
2019-11-04 21:51:03 +0530

    Update pwmRegister.ino

--- pwmRegister/pwmRegister.ino
+++ pwmRegister/pwmRegister.ino
@@ -1,10 +1,11 @@
 void setup(){
 pinMode(3, OUTPUT);
-  pinMode(11, OUTPUT);
+
   TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM20);
   TCCR2B = _BV(WGM22) | _BV(CS22);
   OCR2A = 180;
-  OCR2B = 50;
+  OCR2B = 50; // changing this value affects the duty cycle.
+              // here it is 50% duty cycle
 }
 void loop(){
   }
